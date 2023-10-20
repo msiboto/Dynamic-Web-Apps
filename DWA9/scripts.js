b8041bce4c58fb343e9c20da2221378b0da07b13
michael <m.siboto88@gmail.com>
2023-10-20 08:19:06 +0200

    commit

---
+++ DWA9/scripts.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+import { books, authors, genres, BOOKS_PER_PAGE, html } from './data.js';
+// eslint-disable-next-line
+import BookPreview from './abstract.js';
+
+const bookPreview = new BookPreview()
+
+html.list.items.addEventListener('click', (event) => {
+    bookPreview.bookPreviewShow(event);
+});
+
+
+
+
+
+
+
+
+
+
+
